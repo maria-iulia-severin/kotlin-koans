c1b9ee9c08b6444a2f29af7575dd8665b4fd0e6e
Iulia Severin <iulia.severin12@gmail.com>
2023-09-22 10:58:56 +0300

    Sequences

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,9 +1,9 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
-fun findMostExpensiveProductBy(customer: Customer): Product? {
-    return customer.orders.filter(Order::isDelivered).flatMap(Order::products)
+fun findMostExpensiveProductBy(customer: Customer): Product? =
+     customer.orders.filter(Order::isDelivered).flatMap(Order::products)
         .maxByOrNull(Product::price)
-}
+
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
