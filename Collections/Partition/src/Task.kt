bf0383090e827e11bb5d42b314d7a5169a133cc2
Iulia Severin <iulia.severin12@gmail.com>
2023-09-22 08:51:37 +0300

    Partition

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,5 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = customers.filter {
+    val(deliverd, undeliverd) = it.orders.partition { it.isDelivered }
+    undeliverd.size>deliverd.size
+}.toSet()
